5d091f046ba3f9a3bbbe65eacb5ae36eb47094ef
Serhii Antoniuk <seantoni@cbr3s3.42barcelona.com>
2024-06-20 12:35:11 +0200

    El primer Commit

---
+++ ex08/ft_sort_int_tab.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seantoni <seantoni@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/24 09:34:19 by seantoni          #+#    #+#             */
+/*   Updated: 2023/10/24 12:36:00 by seantoni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	j;
+	int	min;
+
+	i = 0;
+	while (i < size)
+	{
+		j = i + 1;
+		while (j < size)
+		{
+			if (tab[i] > tab[j])
+			{
+				min = tab[i];
+				tab[i] = tab[j];
+				tab[j] = min;
+			}
+			j++;
+		}
+		i++;
+	}
+}
